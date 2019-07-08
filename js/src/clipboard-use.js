3dac1ccda1e61032c39018bba50e40b16589e4ba
hhh <2375740452@qq.com>
2019-07-08 19:03:14 +0800

    Site updated: 2019-07-08 19:02:58

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Sun Jul 07 2019 23:51:25 GMT+0800 (中国标准时间)
+// build time:Mon Jul 08 2019 19:02:55 GMT+0800 (中国标准时间)
 !function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
 //rebuild by neat 
