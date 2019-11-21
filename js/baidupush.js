fd64095cb8fd4e3fd8376290564bd2911ab2d945
hhh <2375740452@qq.com>
2019-11-21 23:41:19 +0800

    Site updated: 2019-11-21 23:41:04

--- js/baidupush.js
+++ js/baidupush.js
@@ -1,3 +1,12 @@
-// build time:Tue Nov 19 2019 23:45:16 GMT+0800 (中国标准时间)
-(function(){var t=document.createElement("script");var e=window.location.protocol.split(":")[0];if(e==="https"){t.src="https://zz.bdstatic.com/linksubmit/push.js"}else{t.src="http://push.zhanzhang.baidu.com/push.js"}var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)})();
-//rebuild by neat 
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
