0bc376e0ecc9e2549da7faba757ccbb25cd4815a
hhh <2375740452@qq.com>
2019-12-01 22:09:58 +0800

    Site updated: 2019-12-01 22:09:50

--- js/src/clipboard-use.js
+++
@@ -1,3 +0,0 @@
-// build time:Fri Nov 29 2019 21:45:18 GMT+0800 (中国标准时间)
-!function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
-//rebuild by neat 
