edff5604c9f36f5b7030a0f33b398b8ef1f82d52
hhh <2375740452@qq.com>
2019-12-03 23:21:28 +0800

    Site updated: 2019-12-03 23:21:12

--- js/baidupush.js
+++ js/baidupush.js
@@ -1,3 +1,3 @@
-// build time:Mon Dec 02 2019 23:11:00 GMT+0800 (中国标准时间)
+// build time:Tue Dec 03 2019 23:19:55 GMT+0800 (中国标准时间)
 (function(){var t=document.createElement("script");var e=window.location.protocol.split(":")[0];if(e==="https"){t.src="https://zz.bdstatic.com/linksubmit/push.js"}else{t.src="http://push.zhanzhang.baidu.com/push.js"}var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)})();
 //rebuild by neat 
