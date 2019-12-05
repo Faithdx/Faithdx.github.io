ec7539cbbd997fdaa007db89faa6a673bf99b790
faithdx <1481087974@qq.com>
2019-12-05 12:23:25 +0800

    Site updated: 2019-12-05 12:23:18

--- js/runtimeshow.js
+++ js/runtimeshow.js
@@ -1,3 +1,3 @@
-// build time:Mon Dec 02 2019 15:36:55 GMT+0800 (GMT+08:00)
+// build time:Thu Dec 05 2019 12:23:11 GMT+0800 (GMT+08:00)
 var start_date=document.getElementById("runtionshow").getAttribute("start_date");function show_date_time(){BirthDay=new Date(start_date);today=new Date;timeold=today.getTime()-BirthDay.getTime();msPerDay=24*60*60*1e3;e_daysold=timeold/msPerDay;daysold=Math.floor(e_daysold);webinfo_runtime_count.innerHTML=daysold+" "+GLOBAL_CONFIG.runtime_unit}var interval;show_date_time();clearInterval(interval);interval=setInterval(show_date_time,1e4);
 //rebuild by neat 
