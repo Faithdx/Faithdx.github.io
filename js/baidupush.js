a15a7bebede5a490ee5c4bd3d80a7356d09468e4
faithdx <1481087974@qq.com>
2019-12-27 18:13:18 +0800

    Site updated: 2019-12-27 18:13:09

--- js/baidupush.js
+++ js/baidupush.js
@@ -1,3 +1,3 @@
-// build time:Thu Dec 26 2019 09:13:15 GMT+0800 (GMT+08:00)
+// build time:Fri Dec 27 2019 18:13:00 GMT+0800 (GMT+08:00)
 (function(){var t=document.createElement("script");var e=window.location.protocol.split(":")[0];if(e==="https"){t.src="https://zz.bdstatic.com/linksubmit/push.js"}else{t.src="http://push.zhanzhang.baidu.com/push.js"}var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)})();
 //rebuild by neat 
