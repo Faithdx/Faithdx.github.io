a4097345adb2d32fab4610bef7e6c7e88fc8fc3f
faithdx <1481087974@qq.com>
2020-01-03 18:33:08 +0800

    Site updated: 2020-01-03 18:33:00

--- js/baidupush.js
+++ js/baidupush.js
@@ -1,3 +1,3 @@
-// build time:Fri Jan 03 2020 18:17:32 GMT+0800 (GMT+08:00)
+// build time:Fri Jan 03 2020 18:32:59 GMT+0800 (GMT+08:00)
 (function(){var t=document.createElement("script");var e=window.location.protocol.split(":")[0];if(e==="https"){t.src="https://zz.bdstatic.com/linksubmit/push.js"}else{t.src="http://push.zhanzhang.baidu.com/push.js"}var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)})();
 //rebuild by neat 
--- js/runtimeshow.js
+++ js/runtimeshow.js
@@ -1,3 +1,3 @@
-// build time:Fri Jan 03 2020 18:17:32 GMT+0800 (GMT+08:00)
+// build time:Fri Jan 03 2020 18:32:59 GMT+0800 (GMT+08:00)
 var start_date=document.getElementById("runtionshow").getAttribute("start_date");function show_date_time(){BirthDay=new Date(start_date);today=new Date;timeold=today.getTime()-BirthDay.getTime();msPerDay=24*60*60*1e3;e_daysold=timeold/msPerDay;daysold=Math.floor(e_daysold);webinfo_runtime_count.innerHTML=daysold+" "+GLOBAL_CONFIG.runtime_unit}var interval;show_date_time();clearInterval(interval);interval=setInterval(show_date_time,1e4);
 //rebuild by neat 
